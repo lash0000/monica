f65d057b120b2b917483970a8217208d094c96a0
callmecla <flores.clarencekyle.manrique@gmail.com>
2025-10-29 01:09:35 +0800

    add traffic route to user routes

--- src/pages/user/routes/UserRoutes.jsx
+++ src/pages/user/routes/UserRoutes.jsx
@@ -1,12 +1,14 @@
 import { Route } from 'react-router-dom';
 import Authenticated_Layout from '../layouts/Authenticated';
 import Dashboard from '../pages/Dashboard';
+import Traffic from '../pages/Traffic';
 import CheckAuth from '../../../lib/CheckAuth';
 
 const UserRoutes = () => [
   // Authenticated Routes
   <Route path="/" element={<CheckAuth><Authenticated_Layout /></CheckAuth>}>
     <Route path="/dashboard" element={<CheckAuth><Dashboard /></CheckAuth>} />
+    <Route path="/traffic" element={<CheckAuth><Traffic /></CheckAuth>} />
   </Route>,
 ]
 
