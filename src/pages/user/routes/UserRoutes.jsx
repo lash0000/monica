0ae45c5d1c45b43b95d05da71a08d65a6a869cf2
callmecla <flores.clarencekyle.manrique@gmail.com>
2025-11-07 00:05:22 +0800

    tinanggal ko muna yung lucide-react sakin, yun kasi turo ni papi gemini ko

--- src/pages/user/routes/UserRoutes.jsx
+++ src/pages/user/routes/UserRoutes.jsx
@@ -11,14 +11,12 @@ import MedicalAppointment from '../pages/MedicalAppointment';
 import Profile from '../pages/Profile';
 import UserTicket from '../pages/UserTicket';
 import Ticket from '../pages/Ticket';
+import PredictiveTickets from '../pages/PredictiveTickets';
 
 const UserRoutes = () => [
   // Authenticated Routes
   <Route path="/" element={<CheckAuth><Authenticated_Layout /></CheckAuth>}>
     <Route path="/dashboard" element={<CheckAuth><Dashboard /></CheckAuth>} />
-<<<<<<< HEAD
-    <Route path="/traffic" element={<CheckAuth><Traffic /></CheckAuth>} />
-=======
     <Route path="/ulat-trapiko" element={<CheckAuth><TrafficAdvisory /></CheckAuth>} />
     <Route path="/e-application" element={<CheckAuth><E_app_Apply /></CheckAuth>} />
     <Route path="/e-application/barangay-clearance" element={<CheckAuth><BarangayClearance /></CheckAuth>} />
@@ -27,7 +25,8 @@ const UserRoutes = () => [
     <Route path="/profile" element={<CheckAuth><Profile /></CheckAuth>} />
     <Route path="/ticket" element={<CheckAuth><Ticket /></CheckAuth>} />
     <Route path="/my-tickets" element={<CheckAuth><UserTicket /></CheckAuth>} />
->>>>>>> d07c5d052d1dccef30e86b3d3ec4a0629d629733
+    <Route path="/traffic" element={<CheckAuth><Traffic /></CheckAuth>} />
+    <Route path="/predictive-tickets" element={<CheckAuth><PredictiveTickets /></CheckAuth>} />
   </Route>,
 ]
 
